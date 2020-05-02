3cd6d0fa6cc1effe8d944dfdce9e24a12247e638
Skorpion-tracer <Skorpion-tracer@yandex.ru>
2020-05-02 23:51:25 +0300

    Добавлен код проекта и раставлены объекты на сцене.

---
+++ FirstPersonShooter/Assets/Scripts/Controller/InputController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+
+namespace Geekbrains
+{
+    public sealed class InputController : BaseController, IExecute
+    {
+        private KeyCode _activeFlashLight = KeyCode.T;
+        public void Execute()
+        {
+            if (!IsActive) return;
+            if (Input.GetKeyDown(_activeFlashLight))
+            {
+                ServiceLocator.Resolve<FlashLightController>().Switch();
+            }
+        }
+    }
+}
---
+++ FirstPersonShooter/Assets/Scripts/View/FlashLightUi.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace Geekbrains
+{
+    public sealed class FlashLightUi : MonoBehaviour
+    {
+        private Text _text;
+        private Slider _slider;
+
+        private void Awake()
+        {
+            _text = GetComponentInChildren<Text>();
+            _slider = GetComponentInChildren<Slider>();
+        }
+
+        public float Text
+        {
+            set => _text.text = $"{value:0.0}";
+        }
+
+        public float SliderValue
+        {
+            set => _slider.value = value;
+        }
+
+        public void SetActiveText(bool value)
+        {
+            _text.gameObject.SetActive(value);
+        }
+    }
+}
