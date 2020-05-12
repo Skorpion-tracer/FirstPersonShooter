bc62d59e48d9f9e4a34108ef2e6e76cc77340240
Skorpion-tracer <Skorpion-tracer@yandex.ru>
2020-05-12 18:38:17 +0300

    ДЗ 3 Урок

---
+++ Model/Pellet.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+
+namespace Geekbrains
+{
+    public sealed class Pellet : Fraction
+    {
+        private Rigidbody _rigidbody;
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            transform.localPosition = new Vector3(Random.Range(-0.4f, 0.4f), Random.Range(-0.4f, 0.4f), 0);
+
+            _rigidbody = GetComponent<Rigidbody>();
+            _rigidbody.AddForce(transform.forward * _currentWeapon.Force, ForceMode.Impulse);
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            var tempObj = collision.gameObject.GetComponent<ISetDamage>();
+
+            if (tempObj != null)
+            {
+                tempObj.SetDamage(new InfoCollision(_curDamage, Rigidbody.velocity));
+                DestroyAmmunition();
+            }
+            
+            DestroyAmmunition(_timeToDestruct);
+        }
+    }
+}
